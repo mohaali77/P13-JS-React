e938e3c4869142ee11c46c7fab951ff7b0c6deb0
mohaali77 <mzr.mohand@gmail.com>
2023-10-07 22:46:42 +0200

    modif

--- argent-bank/src/app.js
+++ argent-bank/src/app.js
@@ -1,7 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 
-
 export default function App() {
   return <Routes>
     <Route path="/" element={<Home />} />
