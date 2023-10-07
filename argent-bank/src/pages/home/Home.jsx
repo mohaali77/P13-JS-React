b6f1e1640a3d50d7308790c177aec91286433760
mohaali77 <mzr.mohand@gmail.com>
2023-10-08 00:37:31 +0200

    Update Home.jsx

--- argent-bank/src/pages/home/Home.jsx
+++ argent-bank/src/pages/home/Home.jsx
@@ -6,17 +6,19 @@ import iconSecurity from '../../img/icon-security.png'
 
 export default function Home() {
     return <>
-        <Features
-            image={iconChat}
-            title='You are our #1 priority'
-            description='Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.' />
-        <Features
-            image={iconMoney}
-            title='More savings means higher rates'
-            description='The more you save with us, the higher your interest rate will be!' />
-        <Features
-            image={iconSecurity}
-            title='Security you can trust        '
-            description='We use top of the line encryption to make sure your data and money is always safe.' />
+        <section className="features">
+            <Features
+                image={iconChat}
+                title='You are our #1 priority'
+                description='Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.' />
+            <Features
+                image={iconMoney}
+                title='More savings means higher rates'
+                description='The more you save with us, the higher your interest rate will be!' />
+            <Features
+                image={iconSecurity}
+                title='Security you can trust        '
+                description='We use top of the line encryption to make sure your data and money is always safe.' />
+        </section >
     </>
 }
