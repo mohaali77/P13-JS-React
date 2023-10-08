b25c477262840d8634fe726e49e3c1cb9d7811a4
mohaali77 <mzr.mohand@gmail.com>
2023-10-08 17:18:26 +0200

    modif

--- argent-bank/src/components/features/features.jsx
+++ argent-bank/src/components/features/features.jsx
@@ -1,7 +1,7 @@
 export default function Features({ image, title, description }) {
     return <>
         <div className="feature-item">
-            <img src={image} alt="" classNameName="feature-icon" />
+            <img src={image} alt="" className="feature-icon" />
             <h3 className="feature-item-title">{title}</h3>
             <p>{description}</p>
         </div>
