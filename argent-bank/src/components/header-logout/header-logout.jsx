8101e0a6ed1097b2bb4524806625ac0aaeb6e568
mohaali77 <mzr.mohand@gmail.com>
2023-10-08 21:48:58 +0200

    Update header-logout.jsx

--- argent-bank/src/components/header-logout/header-logout.jsx
+++ argent-bank/src/components/header-logout/header-logout.jsx
@@ -3,15 +3,19 @@ import { Link } from 'react-router-dom'
 
 export default function HeaderLogout() {
     return <>
-        <nav className="main-nav">
+        <nav class="main-nav">
             <Link to='/' className="main-nav-logo" >
                 <img className="main-nav-logo-image" src={argentBankLogo} alt="" />
                 <h1 className="sr-only">Argent Bank</h1>
             </Link>
             <div>
-                <Link to='/sign-in' className="main-nav-item" >
-                    <i className="fa fa-user-circle"></i>
-                    Sign In
+                <Link to='/user' class="main-nav-item">
+                    <i class="fa fa-user-circle"></i>
+                    Tony
+                </Link>
+                <Link to='/' class="main-nav-item">
+                    <i class="fa fa-sign-out"></i>
+                    Sign Out
                 </Link>
             </div>
         </nav>
