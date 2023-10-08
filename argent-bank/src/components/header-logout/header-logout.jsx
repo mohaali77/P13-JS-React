d76e26eb11363e0b3e64f5fd9a5a88eb74b6da42
mohaali77 <mzr.mohand@gmail.com>
2023-10-08 21:23:59 +0200

    modif

---
+++ argent-bank/src/components/header-logout/header-logout.jsx
@@ -0,0 +1,19 @@
+import argentBankLogo from '../../img/argentBankLogo.png'
+import { Link } from 'react-router-dom'
+
+export default function HeaderLogout() {
+    return <>
+        <nav className="main-nav">
+            <Link to='/' className="main-nav-logo" >
+                <img className="main-nav-logo-image" src={argentBankLogo} alt="" />
+                <h1 className="sr-only">Argent Bank</h1>
+            </Link>
+            <div>
+                <Link to='/sign-in' className="main-nav-item" >
+                    <i className="fa fa-user-circle"></i>
+                    Sign In
+                </Link>
+            </div>
+        </nav>
+    </>
+}
