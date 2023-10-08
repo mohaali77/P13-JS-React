6d51080845dbb4451cfa3e7c633c9c1862588c40
mohaali77 <mzr.mohand@gmail.com>
2023-10-08 17:12:15 +0200

    modif

--- argent-bank/src/components/features/features.jsx
+++ argent-bank/src/components/features/features.jsx
@@ -1,8 +1,8 @@
 export default function Features({ image, title, description }) {
     return <>
-        <div class="feature-item">
-            <img src={image} alt="" className="feature-icon" />
-            <h3 class="feature-item-title">{title}</h3>
+        <div className="feature-item">
+            <img src={image} alt="" classNameName="feature-icon" />
+            <h3 className="feature-item-title">{title}</h3>
             <p>{description}</p>
         </div>
     </>
--- argent-bank/src/components/header/header.jsx
+++ argent-bank/src/components/header/header.jsx
@@ -9,7 +9,7 @@ export default function Header() {
                 <h1 className="sr-only">Argent Bank</h1>
             </Link>
             <div>
-                <Link to='/' className="main-nav-item" >
+                <Link to='/sign-in' className="main-nav-item" >
                     <i className="fa fa-user-circle"></i>
                     Sign In
                 </Link>
