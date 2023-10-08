fe870adbaff7eede4e96979c1863e8d3500021ed
mohaali77 <mzr.mohand@gmail.com>
2023-10-08 20:42:17 +0200

    modif

--- argent-bank/src/pages/user/user.jsx
+++ argent-bank/src/pages/user/user.jsx
@@ -1,5 +1,16 @@
+import Account from "../../components/account/account";
+
 export default function User() {
     return <>
+        <main className="main-user bg-dark">
+            <div class="header">
+                <h1>Welcome back<br />Tony Jarvis!</h1>
+                <button class="edit-button">Edit Name</button>
+            </div>
+            <Account />
+            <Account />
+            <Account />
+        </main>
 
     </>
 }
