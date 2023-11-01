abcd3ed5695ba0be5bee1ea034ad0694f92e18b7
mohaali77 <mzr.mohand@gmail.com>
2023-11-01 10:40:08 +0100

    Update sign-in.jsx

--- argent-bank/src/pages/sign-in/sign-in.jsx
+++ argent-bank/src/pages/sign-in/sign-in.jsx
@@ -1,75 +1,71 @@
 import { Link } from "react-router-dom";
 import HeaderLogin from "../../components/header-login/header-login";
-import { useState, useRef } from "react";
+import { useState } from "react";
 
 export default function SignIn() {
-
-    let showErrorMsg = useRef(false)
-    let showErrorEmailMsg = useRef(false)
-    const [email, setEmail] = useState(''); // Utilisez useState pour gérer l'e-mail
-
-
+    const [showErrorEmailMsg, setShowErrorEmailMsg] = useState(false);
+    const [email, setEmail] = useState('');
 
     function isFormValid(e) {
-        let isFormValid = true
+        e.preventDefault();
 
-        e.preventDefault()
+        let isFormValid = true;
+        const regexEmail = new RegExp(/^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$/);
 
-        console.log(email)
-
-        //on définit notre regex qui définira les conditions de validations du prenom 
-        let regexEmail = new RegExp(/^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$/);
-
-        if (email === '' || regexEmail.test(email) === false) {
-            isFormValid = false
-            showErrorEmailMsg = true
+        if (email === '' || !regexEmail.test(email)) {
+            isFormValid = false;
+            setShowErrorEmailMsg(true);
+        } else {
+            setShowErrorEmailMsg(false);
         }
 
         if (isFormValid === true) {
-            console.log('le formulaire est valide !');
+            console.log('Le formulaire est valide !');
         } else {
-            console.log('le formulaire est invalide !')
-            showErrorMsg.current = true
+            console.log('Le formulaire est invalide !');
         }
 
-        return isFormValid
+        return isFormValid;
     }
 
-    /*// Utilisez useEffect pour effectuer des opérations après le rendu
-    useEffect(() => {
-        // Accédez à l'élément DOM en utilisant la référence
-    }, []);*/
+    return (
+        <>
+            <HeaderLogin />
+            <main className="main bg-dark">
+                <section className="sign-in-content">
+                    <i className="fa fa-user-circle sign-in-icon"></i>
+                    <h1>Sign In</h1>
+                    <form>
+                        <div className="input-wrapper">
+                            <label htmlFor="username">Username</label>
+                            <input
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                type="text"
+                                id="username"
+                            />
+                            {showErrorEmailMsg ? (
+                                <div className="errorLogin">L'e-mail n'est pas valide</div>
+                            ) : (
+                                <div className="errorLogin"></div>
+                            )}
+                        </div>
 
+                        <div className="input-wrapper">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" id="password" />
 
-    return <>
-        <HeaderLogin />
-        <main className="main bg-dark">
-            <section className="sign-in-content">
-                <i className="fa fa-user-circle sign-in-icon"></i>
-                <h1>Sign In</h1>
-                <form>
-                    <div className="input-wrapper">
-                        <label htmlFor="username">Username</label>
-                        <input value={email} onChange={(e) => setEmail(e.target.value)} type="text" id="username" />
-                        {showErrorEmailMsg.current ? <div className="errorEmail">L'email n'est pas valide</div>
-                            : <div className="errorEmail"></div>}
-                    </div>
-                    <div className="input-wrapper">
-                        <label htmlFor="password">Password</label>
-                        <input type="password" id="password" />
-                        {showErrorMsg.current ? <div className="errorLogin">L'email ou le mot de passe est incorrect</div>
-                            : <div className="errorLogin"></div>}
-
-                    </div>
-                    <div className="input-remember">
-                        <input type="checkbox" id="remember-me" />
-                        <label htmlFor="remember-me">Remember me</label>
-                    </div>
-                    <Link to="/user" onClick={isFormValid} className="sign-in-button">Sign In</Link>
-                    { // <button class="sign-in-button">Sign In</button> -->
-                    }
-                </form>
-            </section>
-        </main>
-    </>
+                        </div>
+                        <div className="input-remember">
+                            <input type="checkbox" id="remember-me" />
+                            <label htmlFor="remember-me">Remember me</label>
+                        </div>
+                        <Link to="/user" onClick={isFormValid} className="sign-in-button">
+                            Sign In
+                        </Link>
+                    </form>
+                </section>
+            </main>
+        </>
+    );
 }
