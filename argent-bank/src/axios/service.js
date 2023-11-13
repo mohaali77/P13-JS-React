4154fe5a5e86ab11671f7ad5dcc81f9844cac8b7
mohaali77 <mzr.mohand@gmail.com>
2023-11-13 20:55:48 +0100

    Update service.js

--- argent-bank/src/axios/service.js
+++ argent-bank/src/axios/service.js
@@ -2,17 +2,25 @@ import axios from 'axios';
 
 const API_URL = 'http://localhost:3000/';
 
-export async function loginUser(userId) {
+export async function loginUser() {
     try {
-        const response = await axios.post(`${API_URL}user/${userId}`);
+        const response = await axios.post(`${API_URL}login`);
         return response.data
     } catch (error) {
     }
 }
 
-export async function updateUserProfile(userId) {
+export async function getUserProfile() {
     try {
-        const response = await axios.put(`${API_URL}user/${userId}/activity`);
+        const response = await axios.post(`${API_URL}profile`);
+        return response.data
+    } catch (error) {
+    }
+}
+
+export async function updateUserProfile() {
+    try {
+        const response = await axios.put(`${API_URL}profile`);
         return response.data
     } catch (error) {
     }
