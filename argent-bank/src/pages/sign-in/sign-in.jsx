ec24d7251772a0aeb2238b351858c39c0d817898
mohaali77 <mzr.mohand@gmail.com>
2023-11-15 17:38:07 +0100

    Update sign-in.jsx

--- argent-bank/src/pages/sign-in/sign-in.jsx
+++ argent-bank/src/pages/sign-in/sign-in.jsx
@@ -79,41 +79,41 @@ export default function SignIn() {
     return (
         <>
             {
-                //userState && userState.firstName ? navigate('/user') :
-                <main className="main bg-dark">
-                    <section className="sign-in-content">
-                        <i className="fa fa-user-circle sign-in-icon"></i>
-                        <h1>Sign In</h1>
-                        <form onSubmit={(e) => handleSubmit(e)}>
-                            <div className="input-wrapper">
-                                <label htmlFor="username">Username</label>
-                                <input
-                                    onChange={(e) => setUser(e.target.value)}
-                                    type="text"
-                                    id="username"
-                                    value={user}
-                                    required
-                                />
-                            </div>
+                userState.isConnected ? navigate('/user') :
+                    <main className="main bg-dark">
+                        <section className="sign-in-content">
+                            <i className="fa fa-user-circle sign-in-icon"></i>
+                            <h1>Sign In</h1>
+                            <form onSubmit={(e) => handleSubmit(e)}>
+                                <div className="input-wrapper">
+                                    <label htmlFor="username">Username</label>
+                                    <input
+                                        onChange={(e) => setUser(e.target.value)}
+                                        type="text"
+                                        id="username"
+                                        value={user}
+                                        required
+                                    />
+                                </div>
 
-                            <div className="input-wrapper">
-                                <label htmlFor="password">Password</label>
-                                <input
-                                    type="password"
-                                    id="password"
-                                    onChange={(e) => setPassword(e.target.value)}
-                                    value={password}
-                                    required
-                                />
-                            </div>
-                            <div className="input-remember">
-                                <input type="checkbox" id="remember-me" />
-                                <label htmlFor="remember-me">Remember me</label>
-                            </div>
-                            <button type="submit" className="sign-in-button">Sign In</button>
-                        </form>
-                    </section>
-                </main >
+                                <div className="input-wrapper">
+                                    <label htmlFor="password">Password</label>
+                                    <input
+                                        type="password"
+                                        id="password"
+                                        onChange={(e) => setPassword(e.target.value)}
+                                        value={password}
+                                        required
+                                    />
+                                </div>
+                                <div className="input-remember">
+                                    <input type="checkbox" id="remember-me" />
+                                    <label htmlFor="remember-me">Remember me</label>
+                                </div>
+                                <button type="submit" className="sign-in-button">Sign In</button>
+                            </form>
+                        </section>
+                    </main >
             }
         </>
     );
