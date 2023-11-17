e70808a8f53fcdbb643ba096dea334ca893b7ae6
mohaali77 <mzr.mohand@gmail.com>
2023-11-17 17:10:09 +0100

    modif

--- argent-bank/src/pages/home/Home.jsx
+++ argent-bank/src/pages/home/Home.jsx
@@ -1,6 +1,6 @@
 import Banner from "../../components/banner/banner";
 import Features from "../../components/features/features";
-import HeaderLogin from "../../components/header/header-login";
+import HeaderLogin from "../../components/header/header";
 
 import iconChat from '../../img/icon-chat.png'
 import iconMoney from '../../img/icon-money.png'
