731f616b2e8dbfdf26ae3733116bf67570d7fb50
mohaali77 <mzr.mohand@gmail.com>
2023-11-17 18:08:51 +0100

    Create getUserInState.js

---
+++ argent-bank/src/hook/getUserInState.js
@@ -0,0 +1,35 @@
+// hooks/useFetchUserProfile.js
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { login } from '../features/userSlice';
+import { getUserProfile } from '../axios/service';
+
+export function useFetchUserProfile() {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+
+        if (token) {
+            getUserProfile(token)
+                .then((response) => {
+                    if (response) {
+                        dispatch(
+                            login({
+                                firstName: response.body.firstName,
+                                lastName: response.body.lastName,
+                                id: response.body.id,
+                                email: response.body.email,
+                                password: response.body.password,
+                                token: token,
+                                isConnected: true,
+                            })
+                        );
+                    }
+                })
+                .catch((error) => {
+                    console.error('Erreur lors de la récupération du profil', error);
+                });
+        }
+    }, [dispatch]);
+}
