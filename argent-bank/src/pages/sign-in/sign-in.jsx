98515ad7763dfff1e93739c1a4fbb7c75ce81476
mohaali77 <mzr.mohand@gmail.com>
2023-11-17 18:22:33 +0100

    Update sign-in.jsx

--- argent-bank/src/pages/sign-in/sign-in.jsx
+++ argent-bank/src/pages/sign-in/sign-in.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { login, selectUser } from "../../features/userSlice";
