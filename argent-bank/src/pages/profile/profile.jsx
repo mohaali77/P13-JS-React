97b4c5794eca26aae728ca789222fbd533e0cb16
mohaali77 <mzr.mohand@gmail.com>
2023-11-18 16:27:03 +0100

    Update profile.jsx

--- argent-bank/src/pages/profile/profile.jsx
+++ argent-bank/src/pages/profile/profile.jsx
@@ -19,7 +19,34 @@ export default function Profile() {
             <div class="header">
                 <h1>Welcome back<br />{userState && userState.isConnected ? userState.firstName + ' ' + userState.lastName + ' !' : null}</h1>
                 <button onClick={handleEdit} class="edit-button">Edit Name</button>
+                <form className="edit-name">
+                    <div className="input-wrapper">
+                        <label htmlFor="firstName">Username</label>
+                        <input
+                            //onChange={}
+                            type="text"
+                            id="username"
+                            //value={user}
+                            required
+                        />
+                    </div>
+                    <div className="input-wrapper">
+                        <label htmlFor="lastName">Username</label>
+                        <input
+                            //onChange={}
+                            type="text"
+                            id="username"
+                            //value={user}
+                            required
+                        />
+                    </div>
 
+                    <div className="input-remember">
+                        <input type="checkbox" id="remember-me" />
+                        <label htmlFor="remember-me">Remember me</label>
+                    </div>
+                    <button type="submit" className="sign-in-button">Sign In</button>
+                </form>
             </div>
             <Account title='Argent Bank Checking' description='Available Balance' />
             <Account title='Argent Bank Savings' description='Available Balance' />
