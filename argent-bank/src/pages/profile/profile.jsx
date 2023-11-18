1985bba6044d6aa1dc57f8776b55677602c34bce
mohaali77 <mzr.mohand@gmail.com>
2023-11-18 15:48:33 +0100

    Update profile.jsx

--- argent-bank/src/pages/profile/profile.jsx
+++ argent-bank/src/pages/profile/profile.jsx
@@ -2,17 +2,22 @@ import Account from "../../components/account/account";
 import { GetUserInState } from "../../hook/getUserInState";
 import { selectUser } from "../../features/userSlice";
 import { useSelector } from "react-redux";
+import { hasExpectedRequestMetadata } from "@reduxjs/toolkit/dist/matchers";
 
 
 export default function Profile() {
     const userState = useSelector(selectUser)
     GetUserInState()
 
+    const handleEdit = (e) => {
+
+    }
+
     return <>
         <main className="main-user bg-dark">
             <div class="header">
                 <h1>Welcome back<br />{userState && userState.isConnected ? userState.firstName + ' ' + userState.lastName + ' !' : null}</h1>
-                <button class="edit-button">Edit Name</button>
+                <button onClik={handleEdit} class="edit-button">Edit Name</button>
             </div>
             <Account title='Argent Bank Checking' description='Available Balance' />
             <Account title='Argent Bank Savings' description='Available Balance' />
