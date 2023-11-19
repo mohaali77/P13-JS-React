ab818dae6c11c3c4533dfc614a22a693797e81d4
mohaali77 <mzr.mohand@gmail.com>
2023-11-19 23:49:04 +0100

    s

--- argent-bank/src/pages/profile/profile.jsx
+++ argent-bank/src/pages/profile/profile.jsx
@@ -9,7 +9,7 @@ import { useRef, useState } from "react";
 export default function Profile() {
 
     GetUserInState()
-    saassssss
+    saasssssss
 
     const userState = useSelector(selectUser)
     const editBtn = useRef(null)
