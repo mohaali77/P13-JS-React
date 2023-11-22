10b5e950dfe054ab6c1a668d7a85edbcdd4e63ee
mohaali77 <mzr.mohand@gmail.com>
2023-11-22 16:19:35 +0100

    Update app.js

--- argent-bank/src/app.js
+++ argent-bank/src/app.js
@@ -1,12 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home/home";
 import Profile from './pages/profile/profile'
-import SignIn from './pages/login/login'
+import Login from './pages/login/login'
 
 export default function App() {
   return <Routes>
     <Route path="/" element={<Home />} />
-    <Route path="/login" element={<SignIn />} />
-    <Route path="/profile/:id" element={<User />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/profile/:id" element={<Profile />} />
   </Routes>
 }
