13a852d2c2fdd93c41ff0fe4b934769d5d634c8c
mohaali77 <mzr.mohand@gmail.com>
2023-12-02 17:49:01 +0100

    Update account.jsx

--- argent-bank/src/components/account/account.jsx
+++ argent-bank/src/components/account/account.jsx
@@ -4,10 +4,16 @@ import { selectUser } from "../../features/userSlice";
 export default function Account({ title, description }) {
     const userState = useSelector(selectUser);
 
+    const tonyArray = {
+        number: 'x9444',
+        amount: '1,000'
+
+    }
+
     return <>
         <section className="account">
             <div className="account-content-wrapper">
-                <h3 className="account-title">{title} (x8349)</h3>
+                <h3 className="account-title">{title}</h3>
                 <p className="account-amount">$1,000</p>
                 <p className="account-amount-description">{description}</p>
             </div>
